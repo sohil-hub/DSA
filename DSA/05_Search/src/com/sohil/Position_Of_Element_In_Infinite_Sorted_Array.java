9f624250ff434c49a37064ed7383503f52499f38
sohil-hub <>
2022-06-14 20:56:06 +0530

    Problem binary search

---
+++ DSA/05_Search/src/com/sohil/Position_Of_Element_In_Infinite_Sorted_Array.java
@@ -0,0 +1,39 @@
+package com.sohil;
+
+public class Position_Of_Element_In_Infinite_Sorted_Array {
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 160;
+        int ans = ans(arr, target);
+        System.out.println(ans);
+    }
+
+    private static int ans(int[] arr, int target) {
+        int start=0, end=1;
+
+        while (target > arr[end]){
+            int newStart = end + 1;
+            //end = previous_end + boxSize*2
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+        return binarySearch(arr, target, start, end);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end){
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]){
+                end = mid - 1;
+            }else if(target > arr[mid]){
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
